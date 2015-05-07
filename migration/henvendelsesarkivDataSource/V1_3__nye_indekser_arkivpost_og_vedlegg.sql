cede4af07dfc120e012e19cfdd96e2eaa04670d2
Ketil Velle <ketil.s.velle@nav.no>
2015-05-07 10:42:11 +0200

    Failsafe delete av mulig ekiststerende index

--- migration/henvendelsesarkivDataSource/V1_3__nye_indekser_arkivpost_og_vedlegg.sql
+++ migration/henvendelsesarkivDataSource/V1_3__nye_indekser_arkivpost_og_vedlegg.sql
@@ -1,4 +1,14 @@
-drop index IDX$$_06FB0001;
+Declare
+  COUNT_INDEXES INTEGER;
+BEGIN
+  select count(*)  INTO COUNT_INDEXES
+  from USER_INDEXES
+  where index_name='IDX$$_06FB0001';
+
+  IF COUNT_INDEXES > 0 THEN
+    EXECUTE IMMEDIATE 'Drop index IDX$$_06FB0001';
+  END IF;
+END;
 
 create index aktor_dato_index on ARKIVPOST(aktoerId, mottattdato);
 create index vedlegg_arkivpostid on VEDLEGG(arkivpostid);
