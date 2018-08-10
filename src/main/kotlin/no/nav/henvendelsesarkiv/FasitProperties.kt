f992203d05d288a661d6ad745c7fe7b8bd99bd2c
Richard Borge <Richard.Borge@nav.no>
2018-08-10 15:04:32 +0200

    Lagt til travis

--- src/main/kotlin/no/nav/henvendelsesarkiv/FasitProperties.kt
+++ src/main/kotlin/no/nav/henvendelsesarkiv/FasitProperties.kt
@@ -2,7 +2,10 @@ package no.nav.henvendelsesarkiv
 
 data class FasitProperties(
         val appName: String = getEnvVar("APP_NAME"),
-        val appVersion: String = getEnvVar("APP_VERSION")
+        val appVersion: String = getEnvVar("APP_VERSION"),
+        val dbUrl: String = getEnvVar("HENVENDELSESARKIVDATASOURCE_URL"),
+        val dbUsername: String = getEnvVar("HENVENDELSESARKIVDATASOURCE_USERNAME"),
+        val dbPassword: String = getEnvVar("HENVENDELSESARKIVDATASOURCE_PASSWORD")
 )
 
 fun getEnvVar(name: String, default: String? = null): String =
