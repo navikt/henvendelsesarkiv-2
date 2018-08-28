557aa2296e3721ca055c1dd5f0b3268dd53f304f
Richard Borge <Richard.Borge@nav.no>
2018-08-28 13:36:28 +0200

    Switchet til gson i stedet for jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,10 @@ val khttpVersion = "0.1.0"
 val ktorVersion = "0.9.2"
 val kotlinxVersion = "0.23.3"
 val slf4jVersion = "1.8.0-beta2"
-val jacksonVersion = "2.9.6"
 val springJdbcVersion = "5.0.8.RELEASE"
 val hikariCpVersion = "2.7.8"
 val prometheusVersion = "0.4.0"
+val gsonVersion = "2.8.5"
 
 val mainClass = "no.nav.henvendelsesarkiv.ApplicationKt"
 
@@ -37,13 +37,14 @@ kotlin {
 dependencies {
     compile(kotlin("stdlib"))
     compile("io.ktor:ktor-server-netty:$ktorVersion")
+    compile("io.ktor:ktor-gson:$ktorVersion")
     compile("org.slf4j:slf4j-simple:$slf4jVersion")
-    compile("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     compile("org.springframework:spring-jdbc:$springJdbcVersion")
     compile("com.zaxxer:HikariCP:$hikariCpVersion")
     compile("io.prometheus:simpleclient_hotspot:$prometheusVersion")
     compile("io.prometheus:simpleclient_common:$prometheusVersion")
     compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxVersion")
+    compile("com.google.code.gson:gson:$gsonVersion")
 
     testCompile("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testCompile("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
@@ -89,6 +90,6 @@ val fatJar = task("fatJar", type = Jar::class) {
         attributes["Implementation-Title"] = "Henvendelsesarkiv"
         attributes["Main-Class"] = mainClass
     }
-    from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
+    from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
     with(tasks["jar"] as CopySpec)
 }
