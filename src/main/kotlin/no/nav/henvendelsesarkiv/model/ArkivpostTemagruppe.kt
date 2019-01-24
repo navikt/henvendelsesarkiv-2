a0c7d544ad70f2abc19d035e73e56454094fcda3
Ketil Velle <ketil.s.velle@nav.no>
2019-01-24 12:13:08 +0100

    Revert "Temp-logging for feilsøking"
    This reverts commit e720f77060620064cc30c55d02b7f4134178fb39.

--- src/main/kotlin/no/nav/henvendelsesarkiv/model/ArkivpostTemagruppe.kt
+++ src/main/kotlin/no/nav/henvendelsesarkiv/model/ArkivpostTemagruppe.kt
@@ -1,6 +1,5 @@
 package no.nav.henvendelsesarkiv.model
 
-import org.slf4j.LoggerFactory
 import org.springframework.jdbc.core.RowMapper
 import java.sql.ResultSet
 
@@ -12,20 +11,8 @@ data class ArkivpostTemagruppe(
         val arkivStatusType: ArkivStatusType?
 )
 
-private val logger = LoggerFactory.getLogger(ArkivpostTemagruppeMapper::class.java)
-
 class ArkivpostTemagruppeMapper : RowMapper<ArkivpostTemagruppe> {
-
     override fun mapRow(rs: ResultSet, rowNum: Int): ArkivpostTemagruppe {
-
-        logger.debug("### Skal hente temagruppe ###")
-        logger.debug("arkpostID: " + rs.getLong("arkivpostId").toString() )
-        logger.debug("fodselsnummer: " +  rs.getString("fodselsnummer") )
-        logger.debug("aktoerId: " +   rs.getString("fodselsnummer") )
-        logger.debug("temagruppe: " +   rs.getString("temagruppe") )
-        logger.debug("status: " +   rs.getString("status") )
-        logger.debug("status mapped: " +  rs.getString("status")?.let { ArkivStatusType.valueOf(it)})
-
         return ArkivpostTemagruppe(
                 rs.getLong("arkivpostId").toString(),
                 rs.getString("aktoerId"),
