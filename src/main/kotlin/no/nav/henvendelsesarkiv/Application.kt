bf4fde2f3cdff62fadfc055f8b5f3cdc6290be4f
Richard Borge <Richard.Borge@nav.no>
2019-02-27 14:31:59 +0100

    Kaller kassering i perioder

--- src/main/kotlin/no/nav/henvendelsesarkiv/Application.kt
+++ src/main/kotlin/no/nav/henvendelsesarkiv/Application.kt
@@ -1,23 +1,38 @@
 package no.nav.henvendelsesarkiv
 
+import no.nav.henvendelsesarkiv.db.DatabaseService
 import org.slf4j.LoggerFactory
+import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.schedule
 
+private const val FEM_MINUTTER: Long = 1000 * 60 * 5
 private val log = LoggerFactory.getLogger("henvendelsesarkiv.Application")
 
 val fasitProperties = FasitProperties()
 
 data class ApplicationState(var running: Boolean = true, var initialized: Boolean = false)
 
-fun main(args: Array<String>) {
+fun main() {
     val applicationState = ApplicationState()
     val applicationServer = createHttpServer(applicationState = applicationState)
+    val kasseringstimer = Timer()
 
     Runtime.getRuntime().addShutdownHook(Thread {
         log.info("Shutdown hook called, shutting down gracefully")
+        kasseringstimer.cancel()
         applicationState.initialized = false
         applicationServer.stop(5, 5, TimeUnit.SECONDS)
     })
 
+    startKasseringsjobb(kasseringstimer)
+
     applicationServer.start(wait = true)
+}
+
+private fun startKasseringsjobb(timer: Timer) {
+    timer.schedule(FEM_MINUTTER, FEM_MINUTTER) {
+        DatabaseService().kasserUtgaatteHenvendelser()
+    }
+
 }
