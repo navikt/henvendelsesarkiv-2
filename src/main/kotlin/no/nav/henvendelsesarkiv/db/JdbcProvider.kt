5fd74a4ff575ffc5fcb0c11ea3e00dc6107a78f0
Nicklas Utgaard <nutgaard@gmail.com>
2020-04-14 11:38:22 +0200

    [KAIZEN-0] større connectionpool mot db

--- src/main/kotlin/no/nav/henvendelsesarkiv/db/JdbcProvider.kt
+++ src/main/kotlin/no/nav/henvendelsesarkiv/db/JdbcProvider.kt
@@ -19,7 +19,7 @@ class ConnectionPool private constructor(application: ApplicationProperties) {
         config.jdbcUrl = application.dbUrl
         config.username = application.dbUsername
         config.password = application.dbPassword
-        config.maximumPoolSize = 10
+        config.maximumPoolSize = 40
         config.minimumIdle = 2
         config.connectionTimeout = 1000
         dataSource = HikariDataSource(config)
